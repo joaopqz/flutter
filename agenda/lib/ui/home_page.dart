79c6ec71d1bf0d59a672474e10cb7b83f0bd6284
Joao Queiroz <queirozjoao95@gmail.com>
2020-08-27 20:34:39 -0300

    subir

---
+++ agenda/lib/ui/home_page.dart
@@ -0,0 +1,30 @@
+import 'dart:html';
+
+import 'package:agenda/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
