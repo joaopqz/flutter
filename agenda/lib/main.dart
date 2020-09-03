133eaf82415e4b51c56cafa4f764aed28400d3e7
joaopqz <queirozjoao95@gmail.com>
2020-09-02 22:39:00 -0300

    agenda

--- agenda/lib/main.dart
+++ agenda/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:agenda/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda/ui/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
